91ce74d60dc210af323012956044cc1b1f8d80e4
matveyf97 <matveyf.work@gmail.com>
2026-09-02 15:26:13 +0300

    test123

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+    int x;
+    cin >> x;
+    cout << 100/x << endl ;
     return 0;
 }
