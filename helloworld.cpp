e7f690a1fae3b2eebec0f965d892ea7ade79cfe8
matveyf97 <matveyf.work@gmail.com>
2026-09-02 15:26:48 +0300

    deleted hello world

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    cout << "Hello, World!" << endl;
     int x;
     cin >> x;
-    cout << 100/x << endl ;
+    cout << 100 / x << endl;
     return 0;
 }
